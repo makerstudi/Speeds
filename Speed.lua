d2d8d1cc947d87ffe85d3e7e3bc99ef1c70c13b6
makerstudi <85789595+makerstudi@users.noreply.github.com>
2021-08-29 13:48:11 +0900

    Update Speed.lua

--- Speed.lua
+++ Speed.lua
@@ -10,9 +10,12 @@ local Frame_3 = Instance.new("Frame")
 local UICorner_4 = Instance.new("UICorner")
 local TextLabel = Instance.new("TextLabel")
 local TextButton_2 = Instance.new("TextButton")
+local ImageLabel = Instance.new("ImageLabel")
 local UICorner_5 = Instance.new("UICorner")
-local TextLabel_2 = Instance.new("TextLabel")
+local TextButton_3 = Instance.new("TextButton")
 local UICorner_6 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local UICorner_7 = Instance.new("UICorner")
 
 --Properties:
 
@@ -24,7 +27,7 @@ Frame.BackgroundColor3 = Color3.fromRGB(89, 89, 89)
 Frame.BackgroundTransparency = 0.200
 Frame.BorderSizePixel = 0
 Frame.Position = UDim2.new(-0.294999987, 0, 0.39199999, 0)
-Frame.Size = UDim2.new(0.239571959, 0, 0.214452237, 0)
+Frame.Size = UDim2.new(0.240328953, 0, 0.220411718, 0)
 Frame.Visible = false
 Frame.Active = true
 Frame.Draggable = true
@@ -35,8 +38,8 @@ Frame_2.Parent = Frame
 Frame_2.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
 Frame_2.BackgroundTransparency = 0.020
 Frame_2.BorderSizePixel = 0
-Frame_2.Position = UDim2.new(-0.0220129639, 0, -0.0413333066, 0)
-Frame_2.Size = UDim2.new(0.987073839, 0, 0.970405638, 0)
+Frame_2.Position = UDim2.new(-0.0220131483, 0, -0.0413333476, 0)
+Frame_2.Size = UDim2.new(0.984265625, 0, 0.981521308, 0)
 
 UICorner_2.Parent = Frame_2
 
@@ -55,8 +58,8 @@ UICorner_3.Parent = TextBox
 
 TextButton.Parent = Frame_2
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.Position = UDim2.new(0.11428576, 0, 0.623756707, 0)
-TextButton.Size = UDim2.new(0.767599285, 0, 0.259457886, 0)
+TextButton.Position = UDim2.new(0.114285894, 0, 0.555028439, 0)
+TextButton.Size = UDim2.new(0.767599344, 0, 0.173547551, 0)
 TextButton.Style = Enum.ButtonStyle.RobloxRoundButton
 TextButton.Font = Enum.Font.Cartoon
 TextButton.Text = "Set"
@@ -83,19 +86,38 @@ TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
-TextButton_2.Parent = Frame
-TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 84, 87)
-TextButton_2.BackgroundTransparency = 1.000
-TextButton_2.Position = UDim2.new(0.845664442, 0, -0.0413333476, 0)
-TextButton_2.Size = UDim2.new(0.116588123, 0, 0.127830699, 0)
+TextButton_2.Parent = Frame_2
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.Position = UDim2.new(0.200981855, 0, 0.726849139, 0)
+TextButton_2.Size = UDim2.new(0.594429255, 0, 0.144910783, 0)
+TextButton_2.Style = Enum.ButtonStyle.RobloxRoundButton
 TextButton_2.Font = Enum.Font.Cartoon
-TextButton_2.Text = "X"
-TextButton_2.TextColor3 = Color3.fromRGB(255, 0, 0)
-TextButton_2.TextScaled = true
-TextButton_2.TextSize = 14.000
+TextButton_2.Text = "reset"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextSize = 30.000
 TextButton_2.TextWrapped = true
 
-UICorner_5.Parent = TextButton_2
+ImageLabel.Parent = Frame_2
+ImageLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.Position = UDim2.new(0, 0, 0.00550939143, 0)
+ImageLabel.Size = UDim2.new(0.0807987154, 0, 0.124728054, 0)
+ImageLabel.Image = "rbxassetid://7356002448"
+
+UICorner_5.Parent = ImageLabel
+
+TextButton_3.Parent = Frame
+TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 84, 87)
+TextButton_3.BackgroundTransparency = 1.000
+TextButton_3.Position = UDim2.new(0.845664442, 0, -0.0413333476, 0)
+TextButton_3.Size = UDim2.new(0.116588123, 0, 0.127830699, 0)
+TextButton_3.Font = Enum.Font.Cartoon
+TextButton_3.Text = "X"
+TextButton_3.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextButton_3.TextScaled = true
+TextButton_3.TextSize = 14.000
+TextButton_3.TextWrapped = true
+
+UICorner_6.Parent = TextButton_3
 
 TextLabel_2.Parent = SpeedGui
 TextLabel_2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
@@ -108,39 +130,46 @@ TextLabel_2.TextColor3 = Color3.fromRGB(209, 209, 209)
 TextLabel_2.TextSize = 23.000
 TextLabel_2.TextWrapped = true
 
-UICorner_6.CornerRadius = UDim.new(0, 4)
-UICorner_6.Parent = TextLabel_2
+UICorner_7.CornerRadius = UDim.new(0, 4)
+UICorner_7.Parent = TextLabel_2
 
 -- Scripts:
 
-local function OODW_fake_script() -- TextButton.LocalScript 
+local function DGJVT_fake_script() -- TextButton.LocalScript 
 	local script = Instance.new('LocalScript', TextButton)
 
 	script.Parent.MouseButton1Click:Connect(function()
-		while true do
-			local player = game.Players.LocalPlayer
-			local setspeed = script.Parent.Parent.TextBox.Text
-			local hum = game.Workspace:WaitForChild(player.Name).Humanoid
-			if hum then
-				hum.WalkSpeed = setspeed
-			end
-			wait(0.5)
+		local player = game.Players.LocalPlayer
+		local setspeed = script.Parent.Parent.TextBox.Text
+		local hum = game.Workspace:WaitForChild(player.Name).Humanoid
+		if hum then
+			hum.WalkSpeed = setspeed
 		end
 	end)
 end
-coroutine.wrap(OODW_fake_script)()
-local function FHYN_fake_script() -- TextButton.LocalScript 
-	local script = Instance.new('LocalScript', TextButton)
+coroutine.wrap(DGJVT_fake_script)()
+local function UMYH_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
 
 	script.Parent.MouseButton1Click:Connect(function()
-		script.Parent.Parent.TextButton.Text = "success"
-		wait(2)
-		script.Parent.Parent.TextButton.Text = "Set"
+		local player = game.Players.LocalPlayer
+		local hum = game.Workspace:WaitForChild(player.Name).Humanoid
+		if hum then
+			hum.WalkSpeed = 16
+		end
 	end)
 end
-coroutine.wrap(FHYN_fake_script)()
-local function DPTBY_fake_script() -- TextButton_2.LocalScript 
+coroutine.wrap(UMYH_fake_script)()
+local function KOIWSUM_fake_script() -- TextButton_2.LocalScript 
 	local script = Instance.new('LocalScript', TextButton_2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.TextBox.Text = ""
+	end)
+end
+coroutine.wrap(KOIWSUM_fake_script)()
+local function YNOYY_fake_script() -- TextButton_3.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_3)
 
 	script.Parent.MouseButton1Click:Connect(function()
 		script.Parent.Parent.Position = UDim2.new(0.373,0,0.586,0)
@@ -150,8 +179,8 @@ local function DPTBY_fake_script() -- TextButton_2.LocalScript
 		script.Parent.Parent.Visible = false
 	end)
 end
-coroutine.wrap(DPTBY_fake_script)()
-local function CZRJLU_fake_script() -- SpeedGui.LocalScript 
+coroutine.wrap(YNOYY_fake_script)()
+local function EBVGHX_fake_script() -- SpeedGui.LocalScript 
 	local script = Instance.new('LocalScript', SpeedGui)
 
 	script.Parent.TextLabel.Visible = true
@@ -175,4 +204,4 @@ local function CZRJLU_fake_script() -- SpeedGui.LocalScript
 	wait(1)
 	script.Parent.TextLabel.Visible = false
 end
-coroutine.wrap(CZRJLU_fake_script)()
+coroutine.wrap(EBVGHX_fake_script)()
