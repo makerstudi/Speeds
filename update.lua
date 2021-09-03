6b81111f098e8fdaa19e4f2bd1630b00529829e1
makerstudi <85789595+makerstudi@users.noreply.github.com>
2021-09-03 14:22:24 +0900

    Update update.lua

--- update.lua
+++ update.lua
@@ -205,3 +205,12 @@ local function GENAWKT_fake_script() -- SpeedGui.LocalScript
 	script.Parent.TextLabel.Visible = false
 end
 coroutine.wrap(GENAWKT_fake_script)()
+--[[
+___  ___        _                           _         _   
+|  \/  |       (_)                         (_)       | |  
+| .  . |  __ _  _  _ __    ___   ___  _ __  _  _ __  | |_ 
+| |\/| | / _` || || '_ \  / __| / __|| '__|| || '_ \ | __|
+| |  | || (_| || || | | | \__ \| (__ | |   | || |_) || |_ 
+\_|  |_/ \__,_||_||_| |_| |___/ \___||_|   |_|| .__/  \__|                                          
+						| | 
+]]--						|_|         
